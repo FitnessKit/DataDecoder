492ad843a862f02f15afcf2b914e4c570a8582ab
Kevin A. Hoogheem <kevin@hoogheem.net>
2019-04-14 12:30:02 -0500

    accio

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "DataDecoder",
-    //platforms: [.iOS("8.0"), .macOS("10.10"), .tvOS("9.0"), .watchOS("2.0")],
+    //platforms: [.iOS("10.0"), .macOS("10.10"), .tvOS("9.0"), .watchOS("2.0")],
     products: [
         .library(name: "DataDecoder", targets: ["DataDecoder"])
     ],
